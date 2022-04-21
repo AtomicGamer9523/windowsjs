73afccdc3607d92bbbc7b7f1d9fa8fc0befaa268
Mat <drunkenmonkey4987@gmail.com>
2022-04-21 13:45:52 -0400

    ?

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,6 @@
 //? Free to use ;)
 
-const winjs = require('../src/index');// use 'windowsjs' instead
+const winjs = require('../src/index');// use 'winjsgui' instead
 
 let window = new winjs.Window("Example", [800, 450]);
 
